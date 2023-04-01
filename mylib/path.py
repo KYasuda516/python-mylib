ac68dcb542d8dd18d846b53249425cbd07316575
kanta <yasuda.kanta.516@gmail.com>
2023-04-02 00:24:23 +0900

    fix fix_path func

--- mylib/path.py
+++ mylib/path.py
@@ -81,16 +81,14 @@ def fix_path(
     ]
 
   # Unixマシンの場合
-  if isinstance(path, WindowsPath):
-  # if isinstance(path, PosixPath):
+  if isinstance(path, PosixPath):
     comps = [
       comp.replace(':', new_char)
       for comp in comps
     ]
 
   # Windowsの場合
-  if isinstance(path, PosixPath):
-  # if isinstance(path, WindowsPath):
+  if isinstance(path, WindowsPath):
     import re
     # 不正な文字を置換
     comps = [
