fb25c4c28f0d1c72d28a851d9a43c05b28669eca
kanta <yasuda.kanta.516@gmail.com>
2023-04-03 12:40:40 +0900

    remove time.py

--- mylib/time.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023 Kanta Yasuda (GitHub: @kyasuda516)
-# This software is released under the MIT License, see LICENSE.
-import datetime
-
-def get_timecode() -> str:
-  dt = datetime.datetime.now()
-  tc =  f'{dt.year}{dt.month}{dt.day}{dt.hour}{dt.minute}{dt.second}'
-  return tc
